8a662016d29baaf2da8b8d560bdc69587dfbe7fa
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-08-27 14:06:50 -0400

    refactor: :recycle: add routes created post

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthenticatorController from "../controllers/authenticator/AuthenticatorController";
 import CreateUserController from "../controllers/CreateUser/CreateUserController";
+import ensureAuth from "../middleware/EnsuredToken";
 // import ensureAuth from "../middleware/ensureAuthenticate";
 // import getAllHeroesController from "../UseCases/all_heroes/getAllHeroesController";
 // import AuthenticateController from "../UseCases/authenticate/AuthenticateController";
@@ -9,6 +10,8 @@ const routes = Router();
 
 routes.post("/createAccount", CreateUserController.createUser);
 routes.post("/loginAccount", AuthenticatorController.authenticate);
+
+routes.post("/createdPost", ensureAuth);
 
 // routes.get("/allHeroes", ensureAuth, getAllHeroesController.getHeroes);
 
