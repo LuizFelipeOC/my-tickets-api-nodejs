df0b3b3cca4836d5beca6e20183d5655968c21a5
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-08-27 21:28:39 -0400

    refactor: :recycle: add route of delete in all routes file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import AuthenticatorController from "../controllers/authenticator/AuthenticatorController";
 import CreatedTicketController from "../controllers/CreatedTicket/CreatedTicketController";
 import CreateUserController from "../controllers/CreateUser/CreateUserController";
+import DeleteTicketController from "../controllers/DeleteTicket/DeleteTicketController";
 import ensureAuth from "../middleware/EnsuredToken";
 // import ensureAuth from "../middleware/ensureAuthenticate";
 // import getAllHeroesController from "../UseCases/all_heroes/getAllHeroesController";
@@ -13,6 +14,7 @@ routes.post("/createAccount", CreateUserController.createUser);
 routes.post("/loginAccount", AuthenticatorController.authenticate);
 
 routes.post("/createdPost", ensureAuth, CreatedTicketController.createdTicket);
+routes.delete("/deletePost", ensureAuth, DeleteTicketController.deleteTicket);
 
 // routes.get("/allHeroes", ensureAuth, getAllHeroesController.getHeroes);
 
